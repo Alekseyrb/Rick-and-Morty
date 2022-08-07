80410cb0bc6d38947a804ae189fe76d1fc93c2b4
Aleksey <kravchenko.a.a.rb@gmail.com>
2022-08-07 11:03:21 +0300

    add pages

--- lib/feature/presentation/pages/person_screen.dart
+++ lib/feature/presentation/pages/person_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:rick_and_morty/feature/presentation/widgets/persons_list_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,7 +9,18 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: const Text('Characters'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            color: Colors.white,
+            icon: const Icon(Icons.search),
+          )
+        ],
+      ),
+      body: PersonsList(),
     );
   }
 }
